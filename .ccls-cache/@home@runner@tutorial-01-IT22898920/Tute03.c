fcdf4687d0658f7db4214ad58c9944bc86c3e858
IT22898920 <128301050+IT22898920@users.noreply.github.com>
2023-03-19 16:00:19 +0000

    write the Tute03

--- .ccls-cache/@home@runner@tutorial-01-IT22898920/Tute03.c
+++ .ccls-cache/@home@runner@tutorial-01-IT22898920/Tute03.c
@@ -8,12 +8,22 @@ e.g.
 n -> 100
 sum = 1+2+3+....+ 99+100 = 5050
 
-n -> 1-
+n -> 10
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
+
+  int num, i, sum = 0;
+
+  printf("input the number :");
+  scanf("%d", &num);
+
+  for (i = 0; i <= num; i++) {
+    sum = sum + i;
+  }
+
+  printf("sum of the numbers :%d", sum);
+
   return 0;
 }
-
